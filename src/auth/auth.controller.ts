eaa570fbfe9cabe5ca51a6170c72b046a2fc1636
Saptak3315 <saptakturja2018@gmail.com>
2025-11-02 20:05:00 +0600

    feat: auth service added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  // authService:AuthService
+  constructor(private readonly authService: AuthService){
+    // this.authService= authService
+  }
+
+  @Post('register')
+
+  register(){
+    const result= this.authService.registerUser()
+    return result;
+    // return {message:'User Registered successfully'}
+  }
+}
+
